faa6ed6de39b371e5e9eae7f1917e470d5360ec7
S-keerthika <92565156+S-keerthika@users.noreply.github.com>
2022-09-23 19:43:34 +0530

    Delete background.js

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.extension.onRequest.addListener(function(prediction){
-    if (prediction==1){
-        alert("Warning: Malicious website detected !!");
-    }else if (prediction==-1){
-        alert("Website is good to go.... SAFE!!!");
-    }
-});
