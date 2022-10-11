8c9c75b9cb3c10c2f0f0186303c80cf92dca38f1
S-keerthika <92565156+S-keerthika@users.noreply.github.com>
2022-10-11 11:58:45 +0530

    Add files via upload

--- Extension/background.js
+++ Extension/background.js
@@ -1,7 +1,8 @@
 chrome.extension.onRequest.addListener(function(prediction){
     if (prediction==1){
-        alert("Warning: Malicious website detected !!");
+        alert("Warning: Malicious website detected !!  SQL injections or Malware attacks might be expected !!");
+        alert("Precaution: \n 1. Check the SSL Certificate \n 2. Look for the Website Trust Seal \n 3. Evaluate the Site with Security Tools \n 4. Check the URL");
     }else if (prediction==-1){
-        alert("Website is good to go.... SAFE!!!");
+        alert("Keep going .... It's SAFE!!!");
     }
 });
